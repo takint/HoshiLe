43b01f35fe8dd3429a068991d55bc76d8d0da9d7
Jim Le <taki.lnt@gmail.com>
2019-07-15 20:33:57 -0700

    Init project template

---
+++ inc/Utility/PDOAgent.class.php
@@ -0,0 +1,84 @@
+<?php
+
+class PDOAgent {
+
+    // Connection Details
+    private $host = "";
+    private $user = "";
+    private $password = "";
+    private $dbname = "";
+
+    private $dsn = "";           //Data Source Name
+    private $className = "";     //Name of the class we are mapping with this PDO Agent
+    private $error = "";         //Store any error messages
+    private $stmt = "";          //Stores our statement instance
+    private $pdo = "";           //Store our local instantiation of the PDO driver
+
+    public function __construct(string $className) {
+
+        $this->className = $className;
+
+        $this->dsn = 'mysql:host='. $this->host .';dbname='. $this->dbname;
+
+        $options = array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+
+        try{
+            $this->pdo = new PDO($this->dsn, $this->user, $this->password, $options);
+        }catch (Exception $ex) {
+            $this->error = $ex->getMessage();
+            echo $this->error;
+        }
+    }
+
+    public function query(string $query) {
+        $this->stmt = $this->pdo->prepare($query);
+    }
+
+    public function bind($param, $value, $type = null) {
+        if(is_null($type)) {
+            switch($type) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default: $type = PDO::PARAM_STR;
+            }
+        }
+        $this->stmt->bindValue($param, $value, $type);
+    }
+    
+    public function execute($data = null) {
+        if(is_null($data)) {
+            return $this->stmt->execute();
+        } else {
+            return $this->stmt->execute($data);
+        }
+    }
+
+    public function singleResult() {
+        $this->stmt->setFetchMode(PDO::FETCH_CLASS, $this->className);
+        return $this->stmt->fetch(PDO::FETCH_CLASS);
+    }
+
+    public function getResultSet() {
+        return $this->stmt->fetchAll(PDO::FETCH_CLASS, $this->className);
+    }
+
+    public function rowCount() : int {
+        return $this->stmt->rowCount();
+    }
+    
+    public function lastInsertedId() : int {
+        return $this->pdo->lastInsertedId();
+    }
+}
+
+?>
