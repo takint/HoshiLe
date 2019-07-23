74c2abb4accd2d36d79781a8aa4cc096754ab856
Takanori Hoshi <takanori.hoshi@gmail.com>
2019-07-23 12:24:04 -0700

    create store template

---
+++ inc/Client/view/productList.view.php
@@ -0,0 +1,41 @@
+<main>
+  <div class="py-4">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card mb-3">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/81UdIMh89YL._SL1500_.jpg" alt="" class="img-fluid">
+            <div class="card-body d-flex justify-content-between">
+              <h4 class="card-title">MacBook Air</h4>
+              <button type="button" class="btn btn-secondary">
+                Buy
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-3">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/71DvG2FjM%2BL._SL1500_.jpg" alt="" class="img-fluid">
+            <div class="card-body d-flex justify-content-between">
+              <h4 class="card-title">CyberpowerPC</h4>
+              <button type="button" class="btn btn-secondary">
+                Buy
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-3">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/61Ygdf5VvoL._SL1500_.jpg" alt="" class="img-fluid">
+            <div class="card-body d-flex justify-content-between">
+              <h4 class="card-title">Galaxy A70</h4>
+              <button type="button" class="btn btn-secondary">
+                Buy
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
