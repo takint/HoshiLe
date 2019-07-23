257595fc7f6e198f3f686f6b5159437eed19d72e
Le <len13@student.douglascollege.ca>
2019-07-23 11:40:32 -0700

    Update entity files

--- AdminPage.php
+++ AdminPage.php
@@ -2,13 +2,9 @@
 
 require_once("header.inc.php");
 
-// AdminPage::header();
-// AdminPage::navigator();
-// AdminPage::body();
-// AdminPage::footer();
-
-$prod = new Product();
-
-var_dump($prod);
+AdminPage::header();
+AdminPage::navigator();
+AdminPage::body();
+AdminPage::footer();
 
 ?>
