52b7c688e37c130df3683009ebd271fcdd167eab
Jim Le <taki.lnt@gmail.com>
2019-07-28 16:08:26 -0700

    Update insert update delete for product

--- inc/Admin/ProductController.class.php
+++ inc/Admin/ProductController.class.php
@@ -1,7 +1,7 @@
 <?php
 
 class ProductController {
-    public static function actionResult($action, $sortBy = "", $data = null) {
+    public static function getActionResult($action, $sortBy = "", $data = null) {
         switch($action){
             case "view": 
                 $jproduct = RestClient::call("GET", PRODUCT_API, array("id" => $data));
@@ -24,6 +24,15 @@ class ProductController {
                 $products = array_map('Product::deserialize', $jproducts);
                 AdminPage::productList($products);
             break;
+        }
+    }
+
+    public static function postActionResult($postData) {
+        // Product have id it must be an update action
+        if($postData['productId'] != 0) {
+            RestClient::call("PUT", PRODUCT_API, $productDto);
+        } else { // otherwise, it is an insert action
+            RestClient::call("POST", PRODUCT_API, $productDto);
         }
     }
 }
