a6acdb986f4bfbba130b28e94c82bc88d1463969
Le <len13@student.douglascollege.ca>
2019-08-01 14:11:02 -0700

    Finish customer page list and edit

--- inc/Admin/views/customer-details.view.php
+++ inc/Admin/views/customer-details.view.php
@@ -1,5 +1,5 @@
 <h2>
-  <?= $product->getId() != 0 ? "Editing " . $product->getName() : "Adding"?>
+  <?= $customer->getId() != 0 ? "Editing " . $customer->getName() : "Adding"?>
 </h2>
 <?php if(count($errors) != 0) {?>
   <div class="alert alert-danger">
@@ -11,30 +11,16 @@
     </ul>
   </div>
 <?php } ?>
-<form method="post" action="<?= $_SERVER["PHP_SELF"]."?controller=product&action=".$mode?>">
-  <input type="hidden" name="id" value="<?= $product->getId()?>">
+<form method="post" action="<?= $_SERVER["PHP_SELF"]."?controller=user&action=".$mode?>">
+  <input type="hidden" name="id" value="<?= $customer->getId()?>">
   <div class="form-group">
-    <label for="txtProductName">Product Name</label>
-    <input type="text" value="<?= $product->getName()?>" name="name" class="form-control" id="txtProductName" placeholder="Product Name">
+    <label for="txtCustomerName">Customer Name</label>
+    <input type="text" value="<?= $customer->getName()?>" name="name" class="form-control" id="txtCustomerName" placeholder="Customer Name">
   </div>
   <div class="form-group">
-    <label for="txtProductBrand">Product Brand</label>
-    <input type="text" value="<?= $product->getBrand()?>" name="brand" class="form-control" id="txtProductBrand" placeholder="Brand">
-  </div>
-  <div class="form-group">
-    <label for="txtProductPrice">Product Price</label>
-    <div class="input-group mb-2 mr-sm-2">
-        <div class="input-group-prepend">
-            <div class="input-group-text">$</div>
-        </div>
-        <input type="number" value="<?= $product->getPrice()?>" name="price" class="form-control" id="txtProductPrice" placeholder="Price">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="txtImageUrl">Image URL</label>
-    <input type="text" value="<?= $product->getImageUrl()?>" name="imageUrl" class="form-control" id="txtImageUrl" placeholder="Image Url">
-    <img width="400" src=<?= $product->getImageUrl()?> />
+    <label for="txtCustomerEmail">Customer Email</label>
+    <input readonly type="text" value="<?= $customer->getEmail()?>" name="brand" class="form-control" id="txtCustomerBrand" placeholder="Brand">
   </div>
   <button type="submit" class="btn btn-primary">Save</button>
-  <a href="?controller=product" class="btn btn-secondary">Back to list</a>
+  <a href="?controller=user" class="btn btn-secondary">Back to list</a>
 </form>
