cc4219f0c9ae7341c6ffa8a674c2064b35b97bf4
Jim Le <taki.lnt@gmail.com>
2019-08-05 23:16:51 -0700

    Add javascript for icon

--- inc/Admin/views/footer.view.php
+++ inc/Admin/views/footer.view.php
@@ -15,5 +15,6 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+    <script src="js/main.js"></script>
 </body>
 </html>
